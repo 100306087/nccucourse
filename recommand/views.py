999aa1c6ef68078382a9915b3f492d0eed59dcd8
100306087 <g41903@gmail.com>
2015-05-28 22:12:52 +0800

    first commit

---
+++ recommand/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+
+def hello_world(request):
+    return render(request,
+                  'hello.html',
+                  {'current_time': datetime.now()})
